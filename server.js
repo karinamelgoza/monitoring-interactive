08883a99f789f34b2006833c3bef2ac4348e2856
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-29 10:59:33 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const { Console } = require('console')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+
+
+const port = process.env.PORT || 4545
+app.listen(port, () => {
+    console.log(`server running on ${port}`)
+})
