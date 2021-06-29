3a6761ad8e6c3b84b83c929cb284f66619409d04
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-29 11:30:27 -0700

    adds apm

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const { Console } = require('console')
 const express = require('express')
 const path = require('path')
 const app = express()
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: '5d6d15ad7a9946efbf7b45fa5174faa7',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
